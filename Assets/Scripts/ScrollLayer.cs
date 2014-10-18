d634b735104c483ba3b562b276118c6cc9d2ee74
Fortyseven <fortyseven@gmail.com>
2014-10-18 03:50:31 -0400

    ScrollLayer now relative to root position

---
+++ Assets/Scripts/ScrollLayer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollLayer : MonoBehaviour
+{
+    public float Speed = 1.0f;
+    public bool IsMirrored = false;
+    private float _width, _height;
+    private float _tick;
+    private GameObject _paralaxLayer;
+    private GameObject _player;
+    private int _sortIndex;
+
+    private Vector3 _basePos;
+
+    // leftmost == extents.x
+    // rightmost == -extents.x
+    // center = 0
+
+    void Awake()
+    {
+        _basePos = transform.root.position;
+
+        _width = this.renderer.bounds.extents.x * 2;
+        _height = this.renderer.bounds.extents.y * 2;
+
+        this.transform.position = new Vector3( _width / 2, _basePos.y, _basePos.z );
+
+        _paralaxLayer = new GameObject( this.name + " Paralax Copy" );
+
+        // Just to keep it neat, parent this to my parent
+        _paralaxLayer.transform.parent = this.gameObject.transform;
+
+        SpriteRenderer foo_spr = _paralaxLayer.AddComponent<SpriteRenderer>();
+        SpriteRenderer my_spr = GetComponent<SpriteRenderer>();
+        foo_spr.sprite = my_spr.sprite;
+        foo_spr.sortingOrder = my_spr.sortingOrder;
+
+        _paralaxLayer.transform.position = new Vector3( _width * 1.50f, _basePos.y, _basePos.z );
+
+        _sortIndex = GetComponent<SpriteRenderer>().sortingOrder;
+    }
+
+    void Start()
+    {
+        _player = GameObject.Find("Maude");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _tick += Time.deltaTime * Speed;
+        SetOffset();
+    }
+
+    void SetOffset()
+    {
+        Vector3 pos = new Vector3(-_tick % _width + _basePos.x, 
+                                  _player.transform.position.y + _basePos.y * 0.05f * _sortIndex,
+                                  _basePos.z);
+        transform.position = pos;
+    }
+}
